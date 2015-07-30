54e294e14fb2c3dd605035eca5b4b97e6b0b4f1d
jtschuler <jtschuler007@yahoo.com>
2015-07-30 15:01:06 -0400

    yasss

--- BrickMenuV2.java
+++ BrickMenuV2.java
@@ -26,6 +26,12 @@ public class BrickMenuV2 extends JPanel
    DataLine.Info info;
    Clip clip;
    
+   File file2;
+   AudioInputStream stream2;
+   AudioFormat format2;
+   DataLine.Info info2;
+   Clip clip2;
+   
    JButton[] buttonArray1;
    JButton[] buttonArray2;
       
@@ -65,6 +71,13 @@ public class BrickMenuV2 extends JPanel
       info = new DataLine.Info(Clip.class, format);
       clip = (Clip) AudioSystem.getLine(info);
       clip.open(stream);
+      
+      file2 = new File("trololo.wav");
+      stream2 = AudioSystem.getAudioInputStream(file2);
+      format2 = stream2.getFormat();
+      info2 = new DataLine.Info(Clip.class, format2);
+      clip2 = (Clip) AudioSystem.getLine(info2);
+      clip2.open(stream2);
          
       buttonArray1[0].setEnabled(true);
    }
@@ -80,113 +93,181 @@ public class BrickMenuV2 extends JPanel
       
       public void actionPerformed(ActionEvent e)
       {
-      //          File file = new File("SteelBrickSound.wav");
-      //          AudioInputStream stream = AudioSystem.getAudioInputStream(file);
-      //          AudioFormat format = stream.getFormat();
-      //          DataLine.Info info = new DataLine.Info(Clip.class, format);
-      //          Clip clip = (Clip) AudioSystem.getLine(info);
-      //          clip.open(stream);
       
          switch(myX)
          {
-            case 1: level1 = new BrickLevel01();
+            case 1: 
+               try{level1 = new BrickLevel01();}
+               catch(Exception b){}
                try{resetClip();}
                catch(Exception a){}
                clip.start();
+               clip2.start();
                add(level1, BorderLayout.CENTER);
                level1.requestFocus();
                level1.startTimer();
                buttonArray1[1].setEnabled(true);
                break;
-            case 2: level2 = new BrickLevel02();
+               
+            case 2:
+               try{level2 = new BrickLevel02();}
+               catch(Exception b){}
                try{resetClip();}
                catch(Exception a){}
+               clip2.stop();
+               try{resetClip2v1();}
+               catch(Exception c){}
                clip.start();
+               clip2.start();
                add(level2, BorderLayout.CENTER);
                level2.requestFocus();
                level2.startTimer();
                buttonArray1[2].setEnabled(true);
                break;
-            case 3: level3 = new BrickLevel03();
+               
+            case 3: 
+               try{level3 = new BrickLevel03();}
+               catch(Exception b){}
                try{resetClip();}
                catch(Exception a){}
+               clip2.stop();
+               try{resetClip2v2();}
+               catch(Exception c){}
                clip.start();
+               clip2.start();
                add(level3, BorderLayout.CENTER);
                level3.requestFocus();
                level3.startTimer();
                buttonArray1[3].setEnabled(true);
                break;
-            case 4: level4 = new BrickLevel04();
+               
+            case 4: 
+               try{level4 = new BrickLevel04();}
+               catch(Exception b){}
                try{resetClip();}
                catch(Exception a){}
+               clip2.stop();
+               try{resetClip2v3();}
+               catch(Exception c){}
                clip.start();
+               clip2.start();
                add(level4, BorderLayout.CENTER);
                level4.requestFocus();
                level4.startTimer();
                buttonArray1[4].setEnabled(true);
                break;
-            case 5: level5 = new BrickLevel05();
+               
+            case 5:
+               try{level5 = new BrickLevel05();}
+               catch(Exception b){}
                try{resetClip();}
                catch(Exception a){}
+               clip2.stop();
+               try{resetClip2v4();}
+               catch(Exception c){}
                clip.start();
+               clip2.start();
                add(level5, BorderLayout.CENTER);
                level5.requestFocus();
                level5.startTimer();
                buttonArray1[5].setEnabled(true);
                break;
-            case 6: level6 = new BrickLevel06();
+               
+            case 6:
+               try{level6 = new BrickLevel06();}
+               catch(Exception b){}
                try{resetClip();}
                catch(Exception a){}
+               clip2.stop();
+               try{resetClip2v5();}
+               catch(Exception c){}
                clip.start();
+               clip2.start();
                add(level6, BorderLayout.CENTER);
                level6.requestFocus();
                level6.startTimer();
                buttonArray2[0].setEnabled(true);
                break;
-            case 7: level7 = new BrickLevel07();
+               
+            case 7:
+               try{level7 = new BrickLevel07();}
+               catch(Exception b){}
                try{resetClip();}
                catch(Exception a){}
+               clip2.stop();
+               try{resetClip2v6();}
+               catch(Exception c){}
                clip.start();
+               clip2.start();
                add(level7, BorderLayout.CENTER);
                level7.requestFocus();
                level7.startTimer();
                buttonArray2[1].setEnabled(true);
                break;
-            case 8: level8 = new BrickLevel08();
+               
+            case 8:
+               try{level8 = new BrickLevel08();}
+               catch(Exception b){}
                try{resetClip();}
                catch(Exception a){}
+               clip2.stop();
+               try{resetClip2v7();}
+               catch(Exception c){}
                clip.start();
+               clip2.start();
                add(level8, BorderLayout.CENTER);
                level8.requestFocus();
                level8.startTimer();
                buttonArray2[2].setEnabled(true);
                break;
-            case 9: level9 = new BrickLevel09();
+               
+            case 9:
+               try{level9 = new BrickLevel09();}
+               catch(Exception b){}
                try{resetClip();}
                catch(Exception a){}
+               clip2.stop();
+               try{resetClip2v8();}
+               catch(Exception c){}
                clip.start();
+               clip2.start();
                add(level9, BorderLayout.CENTER);
                level9.requestFocus();
                level9.startTimer();
                buttonArray2[3].setEnabled(true);
                break;
-            case 10: level10 = new BrickLevel10();
+               
+            case 10:
+               try{level10 = new BrickLevel10();}
+               catch(Exception b){}
                try{resetClip();}
                catch(Exception a){}
+               clip2.stop();
+               try{resetClip2v9();}
+               catch(Exception c){}
                clip.start();
+               clip2.start();
                add(level10, BorderLayout.CENTER);
                level10.requestFocus();
                level10.startTimer();
                buttonArray2[4].setEnabled(true);
                break;
-            case 11: level11 = new BrickLevel11();
+               
+            case 11:
+               try{level11 = new BrickLevel11();}
+               catch(Exception b){}
                try{resetClip();}
                catch(Exception a){}
+               clip2.stop();
+               try{resetClip2v10();}
+               catch(Exception c){}
                clip.start();
+               clip2.start();
                add(level11, BorderLayout.CENTER);
                level11.requestFocus();
                level11.startTimer();
                break;
+               
          }
          validate();
          repaint();
@@ -201,5 +282,105 @@ public class BrickMenuV2 extends JPanel
       info = new DataLine.Info(Clip.class, format);
       clip = (Clip) AudioSystem.getLine(info);
       clip.open(stream);
+   }
+   
+   public void resetClip2v1() throws Exception
+   {
+      file2 = new File("TetrisB.wav");
+      stream2 = AudioSystem.getAudioInputStream(file2);
+      format2 = stream2.getFormat();
+      info2 = new DataLine.Info(Clip.class, format2);
+      clip2 = (Clip) AudioSystem.getLine(info2);
+      clip2.open(stream2);
+   }
+   
+   public void resetClip2v2() throws Exception
+   {
+      file2 = new File("TetrisA.wav");
+      stream2 = AudioSystem.getAudioInputStream(file2);
+      format2 = stream2.getFormat();
+      info2 = new DataLine.Info(Clip.class, format2);
+      clip2 = (Clip) AudioSystem.getLine(info2);
+      clip2.open(stream2);
+   }
+   
+   public void resetClip2v3() throws Exception
+   {
+      file2 = new File("TetrisB.wav");
+      stream2 = AudioSystem.getAudioInputStream(file2);
+      format2 = stream2.getFormat();
+      info2 = new DataLine.Info(Clip.class, format2);
+      clip2 = (Clip) AudioSystem.getLine(info2);
+      clip2.open(stream2);
+   }
+   
+   public void resetClip2v4() throws Exception
+   {
+      file2 = new File("TetrisA.wav");
+      stream2 = AudioSystem.getAudioInputStream(file2);
+      format2 = stream2.getFormat();
+      info2 = new DataLine.Info(Clip.class, format2);
+      clip2 = (Clip) AudioSystem.getLine(info2);
+      clip2.open(stream2);
+   }
+   
+   public void resetClip2v5() throws Exception
+   {
+      file2 = new File("TetrisB.wav");
+      stream2 = AudioSystem.getAudioInputStream(file2);
+      format2 = stream2.getFormat();
+      info2 = new DataLine.Info(Clip.class, format2);
+      clip2 = (Clip) AudioSystem.getLine(info2);
+      clip2.open(stream2);
+   }
+   
+   public void resetClip2v6() throws Exception
+   {
+      file2 = new File("TetrisA.wav");
+      stream2 = AudioSystem.getAudioInputStream(file2);
+      format2 = stream2.getFormat();
+      info2 = new DataLine.Info(Clip.class, format2);
+      clip2 = (Clip) AudioSystem.getLine(info2);
+      clip2.open(stream2);
+   }
+   
+   public void resetClip2v7() throws Exception
+   {
+      file2 = new File("TetrisB.wav");
+      stream2 = AudioSystem.getAudioInputStream(file2);
+      format2 = stream2.getFormat();
+      info2 = new DataLine.Info(Clip.class, format2);
+      clip2 = (Clip) AudioSystem.getLine(info2);
+      clip2.open(stream2);
+   }
+   
+   public void resetClip2v8() throws Exception
+   {
+      file2 = new File("TetrisA.wav");
+      stream2 = AudioSystem.getAudioInputStream(file2);
+      format2 = stream2.getFormat();
+      info2 = new DataLine.Info(Clip.class, format2);
+      clip2 = (Clip) AudioSystem.getLine(info2);
+      clip2.open(stream2);
+   }
+   
+   public void resetClip2v9() throws Exception
+   {
+      file2 = new File("TetrisB.wav");
+      stream2 = AudioSystem.getAudioInputStream(file2);
+      format2 = stream2.getFormat();
+      info2 = new DataLine.Info(Clip.class, format2);
+      clip2 = (Clip) AudioSystem.getLine(info2);
+      clip2.open(stream2);
+   }
+   
+   public void resetClip2v10() throws Exception
+   {
+      file2 = new File("TetrisB.wav");
+      stream2 = AudioSystem.getAudioInputStream(file2);
+      format2 = stream2.getFormat();
+      info2 = new DataLine.Info(Clip.class, format2);
+      clip2 = (Clip) AudioSystem.getLine(info2);
+      clip2.open(stream2);
    }
 }
--- DriverBrickMenu.java
+++ DriverBrickMenu.java
@@ -5,13 +5,15 @@ import javax.swing.*;
 
 public class DriverBrickMenu
 {
-   public static void main(String[] args)
+   public static void main(String[] args) throws Exception
    { 
+      ImageIcon icon = new ImageIcon("BrickBreaker.jpg");
       JFrame frame = new JFrame("Brick Breaker: HOMEMADE VERSION!!!");
       frame.setSize(720, 840);
       frame.setLocation(300, 90);
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       frame.setContentPane(new BrickMenuV2());
+      frame.setIconImage(icon.getImage());
       frame.setVisible(true);
    }
 }
